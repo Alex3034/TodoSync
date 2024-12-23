d264013cb118fe4ad99e8bc417737bfa066f8646
Alejandro.H <alex1348wk@gmail.com>
2024-12-23 12:28:58 +0100

    Update ListScreen
    add funs to read tasks to firebase and modify checkbox

--- app/src/main/java/com/todosync/ui/ListScreen.kt
+++ app/src/main/java/com/todosync/ui/ListScreen.kt
@@ -2,33 +2,80 @@ package com.todosync.ui
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.todosync.data.addTaskToFirebase
+import com.todosync.data.Task
+import com.todosync.data.FirebaseInstance
 
 @Composable
-fun ListScreen() {
+fun ListScreen(firebaseInstance: FirebaseInstance) {
+
+    val tasks = firebaseInstance.observeTasks()
 
     Screen() {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(16.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        ) {
-            Button(onClick = {
-                addTaskToFirebase()
-            }) {
-                Text(text = "Agregar Tarea")
+        Scaffold(content = { innerPadding ->
+
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding)
+            ) {
+                LazyColumn(
+                    modifier = Modifier.weight(1f),
+                    verticalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
+                    items(tasks) { task ->
+                        TaskItem(
+                            task = task,
+                            onTaskCheckedChange = { updatedTask ->
+                                firebaseInstance.updateTask(updatedTask)
+                            }
+                        )
+                    }
+                }
+                Button(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    onClick = { firebaseInstance.addTaskToFirebase() }
+                ) {
+                    Text(text = "Agregar Tarea")
+                }
             }
-        }
+        })
     }
 }
 
+@Composable
+fun TaskItem(task: Task, onTaskCheckedChange: (Task) -> Unit) {
+    Card(
+        modifier = Modifier.fillMaxWidth(),
+    ) {
+        Row(
+            modifier = Modifier.padding(16.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(text = task.title, style = MaterialTheme.typography.bodyLarge)
+            Checkbox(
+                checked = task.completed,
+                onCheckedChange = { isChecked ->
+                    onTaskCheckedChange(task.copy(completed = isChecked))
+                }
+            )
+        }
+    }
+}
