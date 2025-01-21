2242dc3ffbd0b91ef26cf19a851945d2a1ea4a94
Alejandro.H <alex1348wk@gmail.com>
2025-01-21 14:07:27 +0100

    Update TaskItem.kt

--- app/src/main/java/com/todosync/ui/TaskItem.kt
+++ app/src/main/java/com/todosync/ui/TaskItem.kt
@@ -9,20 +9,25 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CheckboxDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import com.todosync.domain.Task
@@ -43,10 +48,12 @@ fun TaskItem(
 
     Card(
         modifier = Modifier.fillMaxWidth(),
+        colors = CardDefaults.cardColors(containerColor = Color.Transparent),
     ) {
         Row(
             modifier = Modifier.padding(16.dp),
-            horizontalArrangement = Arrangement.SpaceBetween
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
         ) {
             if (isEditingState) {
                 TextField(
@@ -54,6 +61,11 @@ fun TaskItem(
                     modifier = Modifier
                         .weight(1f)
                         .focusRequester(focusRequester),
+                    colors = TextFieldDefaults.colors(
+                        focusedContainerColor = Color.Transparent,
+                        unfocusedContainerColor = Color.Transparent,
+                        focusedIndicatorColor = Color.Green
+                    ),
                     onValueChange = {
                         editedText = it
                         onEditChange(it)
@@ -67,13 +79,16 @@ fun TaskItem(
                     )
                 )
             } else {
-                Text(text = task.title, style = MaterialTheme.typography.bodyLarge)
+                Text(text = task.title, modifier = Modifier.weight(1f), style = MaterialTheme.typography.bodyLarge)
             }
             IconButton(onClick = { onDeleteClick(task) }) {
                 Icon(imageVector = Icons.Default.Delete, contentDescription = "Delete Task")
             }
             Checkbox(
                 checked = task.completed,
+                colors = CheckboxDefaults.colors(
+                    checkedColor = Color.Green
+                ),
                 onCheckedChange = { isChecked ->
                     onTaskCheckedChange(task.copy(completed = isChecked))
                 }
